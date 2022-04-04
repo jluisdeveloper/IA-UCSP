9a843e65be94c01e51cfd690381049f29f7086aa
jluisdeveloper <jluis.developer@gmail.com>
2022-04-04 01:24:43 -0500

    add auxiliar functions & global varaibles

---
+++ Lab_02/aux_functions.hpp
@@ -0,0 +1,17 @@
+void copyBoard(int originalBoard[8][8], int newBoard[8][8]) {
+  for (int i = 0; i <= 7; i++) {
+    for (int j = 0; j <= 7; j++) {
+      originalBoard[i][j] = newBoard[i][j];
+    }
+  }
+}
+
+int evaluateFunction(int board[8][8]) {
+  int score = 0;
+  for (int i = 0; i <= 7; i++) {
+    for (int j = 0; j <= 7; j++) {
+      score += -board[i][j];
+    }
+  }
+  return score;
+}
--- Lab_02/checkers.cpp
+++ Lab_02/checkers.cpp
@@ -1,10 +1,54 @@
 #include <bits/stdc++.h>
+#include "aux_functions.hpp"
 
 using namespace std;
 
+int board[8][8] = {
+  {0, -1, 0, -1, 0, -1, 0, -1},
+  {-1, 0, -1, 0, -1, 0, -1, 0},
+  {0, -1, 0, -1, 0, -1, 0, -1},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {1, 0, 1, 0, 1, 0, 1, 0},
+  {0, 1, 0, 1, 0, 1, 0, 1},
+  {1, 0, 1, 0, 1, 0, 1, 0}
+};
+
+// Game Setup
+
+bool chooseAlgorithm = 0;     // 0 for minimax, 1 for alpha-beta
+bool chooseColor = 0;         // 0 for white, 1 for black
+
+int humanPiece = 1;
+int computerPiece = -1;
+int maxDepth;
+
+class Node {
+  public:
+    int bestBoard[8][8];
+    int currentBoard[8][8];
+    int depth;
+    bool minimize;
+
+    // constructor
+    Node(int _currentBoard[8][8], int _depth) {
+      copyBoard(currentBoard, bestBoard);
+      depth = _depth;
+      minimize = depth > 2 ? 1 : 0;      
+    }
+
+    // minmax implementation
+    int minMax() {
+      if (depth == maxDepth) {
+        return evaluateFunction(currentBoard);
+      }
+    }
+};
+
+
 int main() {
 
-  cout << "casi me olvido el checkout del nuevo branch :v" << endl;
+  cout << "sintaxys test" << endl;
 
   return 0;
 }
